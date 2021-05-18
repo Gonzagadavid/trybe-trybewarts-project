d44f895a7c45157fd8e5676893d7c28e72b478a6
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-18 18:21:10 -0300

    feito requisito tres

---
+++ script.js
@@ -0,0 +1,16 @@
+const btnLogin = document.getElementById("btn-login");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+const regexEmail = /^[\w\.-]+@\w+\.\w+$/g;
+const regexSenha = /^\d{6}$/g;
+
+function checkFormat(event) {
+  event.preventDefault();
+  if (!regexEmail.test(email.value) || !regexSenha.test(senha.value)) {
+    alert("Login ou senha inválidos.");
+  } else {
+    alert("Olá, Tryber!");
+  }
+}
+
+btnLogin.addEventListener("click", checkFormat);
