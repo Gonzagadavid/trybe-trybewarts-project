413055c6b4fe0481a948da15dee353a88f21c321
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-18 22:42:32 -0300

    feito requisito 18

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const regexEmail = /^[\w.-]+@\w+\.\w+$/g;
 const regexSenha = /^\d{6}$/g;
+const checkInfos = document.getElementById('agreement');
+const btnSubmit = document.getElementById('submit-btn');
 
 function checkFormat(event) {
   event.preventDefault();
@@ -14,3 +16,13 @@ function checkFormat(event) {
 }
 
 btnLogin.addEventListener('click', checkFormat);
+
+function enabledAndDisabled(event) {
+  if (event.target.checked === true) {
+    btnSubmit.disabled = false;
+  } else {
+    btnSubmit.disabled = true;
+  }
+}
+
+checkInfos.addEventListener('change', enabledAndDisabled);
